48373489b52b9b992329c319d9c548a5fe0bb418
Tonio5978 <anthonymarques59@gmail.com>
2025-11-07 13:16:56 +0100

    Change names
    New card with change from calcio to nba

--- src/calcio-live-card.js
+++
@@ -1,6 +0,0 @@
-import "./cards/Classifica/calcio-live-classifica.js";
-import "./cards/Classifica/calcio-live-classifica-editor.js";
-import "./cards/Tutte/calcio-live-matches.js";
-import "./cards/Tutte/calcio-live-matches-editor.js";
-import "./cards/Team/calcio-live-team.js";
-import "./cards/Team/calcio-live-team-editor.js";
---
+++ src/nba-live-card.js
@@ -0,0 +1,6 @@
+import "./cards/Classifica/nba-live-classifica.js";
+import "./cards/Classifica/nba-live-classifica-editor.js";
+import "./cards/Tutte/nba-live-matches.js";
+import "./cards/Tutte/nba-live-matches-editor.js";
+import "./cards/Team/nba-live-team.js";
+import "./cards/Team/nba-live-team-editor.js";
